7f9037ba106c12c2edc83a667b58c176aeeb3030
sandy <pounoussamy.sandy@gmail.com>
2021-06-23 17:35:14 +0100

    Added corsheaders to django project installed apps and middleware in trydjango/settings.py

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "tasks",
     "tiles",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "trydjango.urls"
